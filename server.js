b061b4404187b8efed89ca938a626554d704c0ca
Andrii Moskalenko <Andrii_Moskalenko@epam.com>
2021-01-19 00:03:19 +0200

    server.js edited

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/jira-angular'));
+app.use(express.static(__dirname + '/dist/jira'));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/dist/jira-angular/index.html'))
+    res.sendFile(path.join(__dirname, '/dist/jira/index.html'))
 });
 
 app.listen(process.env.PORT || 8080)
